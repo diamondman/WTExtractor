795b328b8042ffb95d432fbd241e11976b4b836f
Jessy Diamond Exum <jessy.diamondman@gmail.com>
2017-12-20 02:00:08 -0800

    WTPlayer: Fixed formatting.

--- wtplayer/javasrc/com/ms/com/Variant.java
+++ wtplayer/javasrc/com/ms/com/Variant.java
@@ -2,49 +2,49 @@ package com.ms.com;
 
 public class Variant {
     public Variant(int i) {
-		// TODO Auto-generated constructor stub
-	}
+        // TODO Auto-generated constructor stub
+    }
 
-	public Variant(double doubleValue) {
-		// TODO Auto-generated constructor stub
-	}
-	public Variant(String string) {
-		// TODO Auto-generated constructor stub
-	}
-	public Variant(boolean booleanValue) {
-		// TODO Auto-generated constructor stub
-	}
-	public Variant() {
-		// TODO Auto-generated constructor stub
-	}
+    public Variant(double doubleValue) {
+        // TODO Auto-generated constructor stub
+    }
+    public Variant(String string) {
+        // TODO Auto-generated constructor stub
+    }
+    public Variant(boolean booleanValue) {
+        // TODO Auto-generated constructor stub
+    }
+    public Variant() {
+        // TODO Auto-generated constructor stub
+    }
 
-	public short getvt(){
-		return 0;
-	}
-    
+    public short getvt(){
+        return 0;
+    }
+
     public boolean getBoolean(){
-		return false;
-	}
+        return false;
+    }
     public double getDouble(){
-		return 0.0;
-	}
+        return 0.0;
+    }
     public float getFloat(){
-		return 0.0f;
-	}
+        return 0.0f;
+    }
     public int getInt(){
-		return 0;
-	}
+        return 0;
+    }
     public String getString(){
-		return null;
-	}
+        return null;
+    }
 
-	public SafeArray toSafeArray() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public SafeArray toSafeArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public void putSafeArray(SafeArray safeArray) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void putSafeArray(SafeArray safeArray) {
+        // TODO Auto-generated method stub
+
+    }
 }
